7c65125e685bb385f3369075a27bf7f28b64e9d9
Julian Coleman <jcoleman@techempower.com>
2019-02-05 17:45:13 -0800

    change background color of header on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
+// Use `var` instead of `const` for smaller OTW.
+
+// I typically have only one header on the page at a time,
+// which is why I'm okay with making a selector this broad.
+var header = document.querySelector("header");
+
 window.onscroll = handleScroll;
 
 function handleScroll() {
-  console.log(window.innerHeight);
+  var currentScrollPosition = window.scrollY;
+  var windowHeight = window.innerHeight;
+  var offsetPercentage = currentScrollPosition / windowHeight;
+
+  header.style.background = `rgba(255, 255, 255, ${offsetPercentage})`;
 }
